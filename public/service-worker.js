e361e335e5688befe553265611dbffdf8f34c2b3
Sajees XPS <sajees.jeya@gmail.com>
2022-04-04 16:12:59 -0400

    fixed issue

---
+++ public/service-worker.js
@@ -0,0 +1,57 @@
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/icons/icon-72x72.png",
+  "/icons/icon-96x96.png",
+  "/icons/icon-128x128.png",
+  "/icons/icon-144x144.png",
+  "/icons/icon-152x152.png",
+  "/icons/icon-192x192.png",
+  "/icons/icon-384x384.png",
+  "/icons/icon-512x512.png",
+  "/js/idb.js",
+  "/js/index.js"
+];
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      return request || fetch(e.request)
+    })
+  );
+});
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+          console.log('installing cache: ' + CACHE_NAME)
+          return cache.addAll(FILES_TO_CACHE)
+      })
+  )
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+      caches.keys().then(function(keyList) {
+          let cacheKeepList = keyList.filter(function(key) {
+              return key.indexOf(APP_PREFIX);
+          });
+          cacheKeepList.push(CACHE_NAME);
+
+          return Promise.all(
+              keyList.map(function(key, i) {
+                  if (cacheKeepList.indexOf(key) === -1){
+                      console.log('deleting cache: ' + keyList[i]);
+                      return caches.delete(keyList[i]);
+                  }
+              })
+          );
+      })
+  );
+});
