a3c308d07399a23fbb0e5d9144d05c983f10a07d
Sajees XPS <sajees.jeya@gmail.com>
2022-04-03 20:16:29 -0400

    fixed file

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,153 +1,76 @@
-let transactions = [];
-let myChart;
+let db;
+const request = indexedDB.open('budget', 1);
 
-fetch("/api/transaction")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    // save db data on global variable
-    transactions = data;
+// if the database version changes
+request.onupgradeneeded = function(event) {
+  // save a reference to the database 
+  const db = event.target.result;
+  // create an object store (table) called `new_budget_entry`
+  db.createObjectStore('new_budget_entry', { autoIncrement: true });
+};
 
-    populateTotal();
-    populateTable();
-    populateChart();
-  });
+request.onsuccess = function(event) {
+  // when db is successfully created save reference to db in global variable
+  db = event.target.result;
 
-function populateTotal() {
-  // reduce transaction amounts to a single total value
-  let total = transactions.reduce((total, t) => {
-    return total + parseInt(t.value);
-  }, 0);
-
-  let totalEl = document.querySelector("#total");
-  totalEl.textContent = total;
-}
-
-function populateTable() {
-  let tbody = document.querySelector("#tbody");
-  tbody.innerHTML = "";
-
-  transactions.forEach(transaction => {
-    // create and populate a table row
-    let tr = document.createElement("tr");
-    tr.innerHTML = `
-      <td>${transaction.name}</td>
-      <td>${transaction.value}</td>
-    `;
-
-    tbody.appendChild(tr);
-  });
-}
-
-function populateChart() {
-  // copy array and reverse it
-  let reversed = transactions.slice().reverse();
-  let sum = 0;
-
-  // create date labels for chart
-  let labels = reversed.map(t => {
-    let date = new Date(t.date);
-    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-  });
-
-  // create incremental values for chart
-  let data = reversed.map(t => {
-    sum += parseInt(t.value);
-    return sum;
-  });
-
-  // remove old chart if it exists
-  if (myChart) {
-    myChart.destroy();
+  // check if app is online, if yes run uploadBudgetEntry() function to send all local db data to api
+  if (navigator.onLine) {
+    uploadBudgetEntry();
   }
+};
 
-  let ctx = document.getElementById("myChart").getContext("2d");
+request.onerror = function(event) {
+  console.log(event.target.errorCode);
+};
 
-  myChart = new Chart(ctx, {
-    type: 'line',
-      data: {
-        labels,
-        datasets: [{
-            label: "Total Over Time",
-            fill: true,
-            backgroundColor: "#6666ff",
-            data
-        }]
+// Executed if attempt to submit a new entry and there's no internet
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions 
+  const transaction = db.transaction(['new_budget_entry'], 'readwrite');
+
+  // access the object store for `new_budget_entry`
+  const budgetObjectStore = transaction.objectStore('new_budget_entry');
+
+  budgetObjectStore.add(record);
+    alert("Offline Mode: Update saved successfully");
+}
+
+function uploadBudgetEntry() {
+  const transaction = db.transaction(['new_budget_entry'], 'readwrite');
+
+  const budgetObjectStore = transaction.objectStore('new_budget_entry');
+
+  const getAll = budgetObjectStore.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+
+          const transaction = db.transaction(['new_budget_entry'], 'readwrite');
+          const budgetObjectStore = transaction.objectStore('new_budget_entry');
+         
+          budgetObjectStore.clear();
+
+          alert("Update: All pending offline transactions have been posted!");
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
-  });
-}
-
-function sendTransaction(isAdding) {
-  let nameEl = document.querySelector("#t-name");
-  let amountEl = document.querySelector("#t-amount");
-  let errorEl = document.querySelector(".form .error");
-
-  // validate form
-  if (nameEl.value === "" || amountEl.value === "") {
-    errorEl.textContent = "Missing Information";
-    return;
-  }
-  else {
-    errorEl.textContent = "";
-  }
-
-  // create record
-  let transaction = {
-    name: nameEl.value,
-    value: amountEl.value,
-    date: new Date().toISOString()
   };
-
-  // if subtracting funds, convert amount to negative number
-  if (!isAdding) {
-    transaction.value *= -1;
-  }
-
-  // add to beginning of current array of data
-  transactions.unshift(transaction);
-
-  // re-run logic to populate ui with new record
-  populateChart();
-  populateTable();
-  populateTotal();
-  
-  // also send to server
-  fetch("/api/transaction", {
-    method: "POST",
-    body: JSON.stringify(transaction),
-    headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
-    }
-  })
-  .then(response => {    
-    return response.json();
-  })
-  .then(data => {
-    if (data.errors) {
-      errorEl.textContent = "Missing Information";
-    }
-    else {
-      // clear form
-      nameEl.value = "";
-      amountEl.value = "";
-    }
-  })
-  .catch(err => {
-    // fetch failed, so save in indexed db
-    saveRecord(transaction);
-
-    // clear form
-    nameEl.value = "";
-    amountEl.value = "";
-  });
 }
 
-document.querySelector("#add-btn").onclick = function() {
-  sendTransaction(true);
-};
+window.addEventListener('online', uploadBudgetEntry);
 
-document.querySelector("#sub-btn").onclick = function() {
-  sendTransaction(false);
-};
